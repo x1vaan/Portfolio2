2696e73babd9c0f70bd5a179b0444ac70ed3bfbf
x1vaan <ivanx467@gmail.com>
2022-10-11 21:36:38 -0300

    fix/navbar

--- ivan-garcia/src/NavBar/NavBar.tsx
+++ ivan-garcia/src/NavBar/NavBar.tsx
@@ -1,18 +1,31 @@
-import React from "react";
+import React,{useState} from "react";
 import Navcss from './NavBar.module.css';
 import logo from '../images/tg.svg';
 import { Link } from 'react-scroll';
 
  
 export default function NavBar (): JSX.Element {
+  const [buttonState, setButtonState] = useState<string>('desactive')
+  const onclick = ()  => {
+       buttonState === 'active' ? setButtonState('desactive') : setButtonState('active') 
+  }
    return (
     <nav className={Navcss.nav}>
       <img src={logo} alt="Image not found" className={Navcss.img}/>
+      <a href="#" className={Navcss.button} onClick={onclick}>
+     <span className={Navcss.bar}></span>
+     <span className={Navcss.bar}></span>
+     <span className={Navcss.bar}></span>
+      </a>
+
+      <div className={buttonState === 'active' ? Navcss.navlistactive : Navcss.navlist}>
       <ul className={Navcss.list}>
        <Link to="about" smooth={true} duration={500}> <li>About</li></Link>  
        <Link to="projects" smooth={true} duration={500}> <li>Projects</li></Link>  
        <Link to="contactme" smooth={true} duration={500}> <li>Contact</li></Link>  
       </ul>
+      </div>
+
     </nav>
    )
 }
